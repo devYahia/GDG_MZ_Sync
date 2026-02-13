d8da1aac571ee9a89075cfa75639338190f9ede0
Yahia Zakaria <mrzak051@gmail.com>
2026-02-13 22:35:53 +0000

    fix: remove unused user variable in middleware to fix build

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -31,9 +31,7 @@ export async function updateSession(request: NextRequest) {
     // supabase.auth.getUser(). A simple mistake can make it very hard to debug
     // issues with users being logged out.
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
+    await supabase.auth.getUser()
 
     return supabaseResponse
 }

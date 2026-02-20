14bfccc7d4019b51e1003615ed01bfb022494bc3
Yahia Zakaria <mrzak051@gmail.com>
2026-02-20 03:59:16 +0000

    fix(sitemap): update /interview route instead of /interview/setup

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -23,7 +23,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 0.8,
         },
         {
-            url: `${appUrl}/interview/setup`,
+            url: `${appUrl}/interview`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.9,
